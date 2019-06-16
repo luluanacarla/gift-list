7ded59fe325806332af668c68bf580c2a57547b3
Luana <luluanacarla@gmail.com>
2019-06-16 15:24:18 -0300

    feat(subHeader): Add subHeader

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './layouts/Header';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import SubHeader from './layouts/SubHeader';
 
 library.add(fas);
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div id="content" className="app-content box-shadow-z0" role="main">
       <Header />
+      <SubHeader />
       <div id="view" className="app-body">
         teste
       </div>
---
+++ src/layouts/SubHeader.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class SubHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  render() {
+    return (
+      <div className="app-header blue shadow text-center">
+        <Row>
+          <Col>
+            <button className="btn btn-link text-subheader">
+              <FontAwesomeIcon icon={'angle-double-left'} fixedWidth />
+              Back
+            </button>
+          </Col>
+          <Col>
+            <div className="text-subheader">
+              <h4>Step 2</h4>
+              <small className="d-block">Descição da etapa 2</small>
+            </div>
+          </Col>
+          <Col>
+            <button className="btn btn-link text-subheader">
+              Next
+              <FontAwesomeIcon icon={'angle-double-right'} fixedWidth />
+            </button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default SubHeader;
